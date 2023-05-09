8c36d4826d620a9f2df8767510007905581b0a37
SF0043 <jin.ping@saxofintech.com>
2023-05-09 16:22:29 +0800

    token pass

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,6 +29,9 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -52,6 +55,11 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal) //该字面量是关键字/标识符
+			return tok
+		} else if isDigit(l.ch) { //数字（整数）的词法单元
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
 			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
@@ -77,6 +85,26 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 跳过连续的空格 换行等 (多次跳过 字符for)
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// 判断是否是数字
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
