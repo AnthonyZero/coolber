0f4be5f6d3784ce72cd243bbb6f8c0dccf664d45
AnthonyZero <736252868@qq.com>
2023-05-11 17:28:36 +0800

    添加repl

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"coolber/repl"
 	"fmt"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Printf("hello world\n")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Coolber programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
 }
---
+++ repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"bufio"
+	"coolber/lexer"
+	"coolber/token"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		//循环 直到结束
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
